9719954f2be7d88592c2786a5b22efc01d9aa9f2
Jack Baron <jackmbaron@gmail.com>
2018-04-06 05:57:19 +0100

    Fix hardcoded board

--- src/index.js
+++ src/index.js
@@ -6,11 +6,11 @@ const fetchThreadJSON = async url => {
   let [, board, index] = url.match(/http:\/\/boards\.4chan\.org\/([a-z]+)\/thread\/([0-9]+)\/?/)
   let { text } = await get(`http://a.4cdn.org/${board}/thread/${index}.json`)
   let { posts } = JSON.parse(text)
-  return posts
+  return { board, posts }
 }
 
-const filterPosts = posts => posts.filter(x => x.tim !== undefined)
-  .map(x => `http://i.4cdn.org/h/${x.tim}${x.ext}`)
+const filterPosts = (posts, board) => posts.filter(x => x.tim !== undefined)
+  .map(x => `http://i.4cdn.org/${board}/${x.tim}${x.ext}`)
 
 const saveAll = async (posts, directory) => {
   await fs.ensureDir(directory)
@@ -22,7 +22,7 @@ const saveAll = async (posts, directory) => {
 }
 
 const downloadThread = (url, directory) => fetchThreadJSON(url)
-  .then(filterPosts)
+  .then(x => filterPosts(x.posts, x.board))
   .then(posts => saveAll(posts, directory))
 
 module.exports = { downloadThread }
